685f0ccfab023b046742f622cb60540a7e2ece74
Sunita-Gurau <sunitagurau.1999@gmail.com>
2024-09-25 16:08:01 +0545

    slicing of dashboard header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header> 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-  </header>
-  <RouterView />
+<div class="flex">
+  <div class="w-1/6"><SideNav/></div>
+  <div class="w-5/6 bg-gray-1 min-h-screen">
+    <div class="m-6 flex justify-between">
+      <JbHeader headingLevel="h2" text="Dashboard"/>
+      <div class="flex items-center justify-center">
+        <div class="relative mr-4">
+          <JbIcon iconName="NotificationBell" />
+          <div class="bg-red-500 w-4 h-4 rounded-full absolute -top-[3px] -right-[3px] text-center text-white text-13px">4</div>
+        </div>
+      <div class="relative ">
+        <JbAvatar
+          size="xl"
+          fullName="Sunita Gurau"
+          />
+<div class="flex items-center justify-center bg-white w-5 h-5 rounded-full absolute -bottom-[6px] right-[4px] text-center text-white text-13px">
+  <div class="bg-green-500 w-3 h-3 rounded-full  "></div>
+
+</div>      </div>
+     </div>
+    </div>
+    <RouterView />
+  </div>
+</div>
 </template>
 
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import SideNav from "../src/components/SideNav.vue";
+import JbHeader from './components/SharedComponents/JbHeader.vue';
+import JbIcon from "./components/SharedComponents/JbIcon.vue";
+import JbAvatar from "./components/SharedComponents/JbAvatar.vue";
+</script>
